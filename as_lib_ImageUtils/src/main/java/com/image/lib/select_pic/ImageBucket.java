2ebcf7c0819ee4c5631474abd796eea940e1decb
wei120698598 <wei120698598@1bf33f2a-9e33-4782-a553-9e545a26ca23>
2016-07-15 09:08:03 +0000

    添加图片选择库，工具库，自定义布局、View库

---
+++ as_lib_ImageUtils/src/main/java/com/image/lib/select_pic/ImageBucket.java
@@ -0,0 +1,10 @@
+package com.image.lib.select_pic;
+
+import java.util.List;
+
+public class ImageBucket {
+	public int count = 0;
+	public String bucketName;
+	public List<ImageItem> imageList;
+
+}
