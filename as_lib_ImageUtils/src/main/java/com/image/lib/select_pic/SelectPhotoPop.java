d6547eafa6108d6e3bafed4f4ea7ae52226f788f
wei120698598 <wei120698598@1bf33f2a-9e33-4782-a553-9e545a26ca23>
2016-07-15 09:08:03 +0000

    添加图片选择库，工具库，自定义布局、View库

---
+++ as_lib_ImageUtils/src/main/java/com/image/lib/select_pic/SelectPhotoPop.java
@@ -0,0 +1,192 @@
+package com.image.lib.select_pic;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.image.lib.R;
+import com.image.lib.select_pic.photoview.ImagePicker;
+import com.image.lib.select_pic.ui.ImageFileActivity;
+
+
+public class SelectPhotoPop {
+    private PopupWindow pop;
+    private LinearLayout ll_popup;
+    public static Uri photoUri;
+    public final static int TAKE_PICTURE = 0x000001;
+
+    public static final int CUSTOMER_ALUM = 1;
+    public static final int NATIVE_ALUM = 0;
+
+    /**
+     * 初始化popupWindow
+     * @param context
+     * @param alum_type 传递CUSTOMER_ALUM = 1使用自定义图片选择器并可同时选择多张，传递NATIVE_ALUM调用系统图片选择器只能一次选一张
+     * @param clazz
+     */
+    public SelectPhotoPop(final Context context, final int alum_type, Class clazz) {
+        initPop(context, alum_type, clazz);
+    }
+
+    /**
+     * 初始化图片选器
+     * @param context
+     * @param flag
+     * @param clazz
+     */
+    private void initPop(final Context context, final int flag, Class clazz) {
+
+        if (flag == CUSTOMER_ALUM) {
+            Bimp.bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.roominfo_add_btn_normal);
+            Bimp.tempSelectBitmap.clear();
+            Bimp.max = 0;
+            Bimp.clazz = clazz;
+        }
+
+        pop = new PopupWindow(context);
+        View view = View.inflate(context, R.layout.pop_item_camera, null);
+        final Animation animation = AnimationUtils.loadAnimation(context, R.anim.activity_translate_out);
+        animation.setFillAfter(true);
+        ll_popup = (LinearLayout) view.findViewById(R.id.ll_popup);
+        ll_popup.startAnimation(AnimationUtils.loadAnimation(context, R.anim.activity_translate_in));
+        pop.setWidth(LayoutParams.MATCH_PARENT);
+        pop.setHeight(LayoutParams.WRAP_CONTENT);
+        pop.setBackgroundDrawable(new BitmapDrawable());
+        pop.setFocusable(true);
+        pop.setOutsideTouchable(true);
+        pop.setContentView(view);
+
+        RelativeLayout parent = (RelativeLayout) view.findViewById(R.id.parent);
+        Button bt1 = (Button) view.findViewById(R.id.item_popupwindows_camera);
+        Button bt2 = (Button) view.findViewById(R.id.item_popupwindows_Photo);
+        Button bt3 = (Button) view.findViewById(R.id.item_popupwindows_cancel);
+        // 整个线性布局
+        parent.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                animation.setAnimationListener(new SelectPhotoPop.MyAnimationListener() {
+
+                    @Override
+                    public void a() {
+                        pop.dismiss();
+                    }
+                });
+                ll_popup.startAnimation(animation);
+            }
+        });
+        // 拍照
+        bt1.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                animation.setAnimationListener(new SelectPhotoPop.MyAnimationListener() {
+
+                    @Override
+                    public void a() {
+                        pop.dismiss();
+                        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+                            photoUri = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    new ContentValues());
+                            // if (flag == 1) {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                            ((Activity) context).startActivityForResult(intent, ImagePicker.PHOTO_CAMERA_CODE);
+                            ((Activity) context).overridePendingTransition(R.anim.activity_translate_in, R.anim.activity_translate_out);
+                            // } else {
+                            // photoTakePath =
+                            // ImagePicker.PickerFromCamera((Activity)
+                            // context,
+                            // android.os.Environment.getExternalStorageState()
+                            // + "image");
+                            // }
+                        } else {
+                            Toast.makeText(context, "没有内存卡不能拍照", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                ll_popup.startAnimation(animation);
+            }
+        });
+        // 相册
+        bt2.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                animation.setAnimationListener(new SelectPhotoPop.MyAnimationListener() {
+
+                    @Override
+                    public void a() {
+                        pop.dismiss();
+                        if (flag == CUSTOMER_ALUM) {// 自定义相册
+                            ((Activity) context).startActivityForResult(new Intent(context, ImageFileActivity.class), 0);
+                            ((Activity) context).overridePendingTransition(R.anim.activity_translate_in, R.anim.activity_translate_out);
+                        } else {
+                            ImagePicker.PickerFromAlbum((Activity) context);
+                        }
+                    }
+                });
+                ll_popup.startAnimation(animation);
+
+            }
+        });
+        // 取消
+        bt3.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                animation.setAnimationListener(new SelectPhotoPop.MyAnimationListener() {
+
+                    @Override
+                    public void a() {
+                        pop.dismiss();
+                    }
+                });
+                ll_popup.startAnimation(animation);
+            }
+        });
+    }
+
+    private abstract class MyAnimationListener implements AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            a();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        public abstract void a();
+    }
+
+    /**
+     * 显示图片选择器在parentView底部
+     * @param context
+     * @param parentView
+     */
+    public void showPop(Context context, View parentView) {
+        ll_popup.startAnimation(AnimationUtils.loadAnimation(context, R.anim.activity_translate_in));
+        pop.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);
+    }
+
+    public String photoTakePath;
+
+}
---
+++ as_lib_ImageUtils/src/main/java/com/image/lib/show_pic/image/ImageDetailFragment.java
@@ -0,0 +1,163 @@
+package com.image.lib.show_pic.image;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.image.lib.R;
+import com.image.lib.select_pic.photoview.PhotoViewAttacher;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * 单张图片显示Fragment
+ */
+public class ImageDetailFragment extends Fragment {
+	private String mImageUrl;
+	private ImageView mImageView;
+	private ProgressBar progressBar;
+	private PhotoViewAttacher mAttacher;
+
+	// 背景图片
+	private ImageView iv_image_bg;
+	private ReleaseBitmapLoadingListener releaseBitmapLoadingListener;
+	private ImageLoader imageLoader;
+
+	public static ImageDetailFragment newInstance(String imageUrl) {
+		final ImageDetailFragment f = new ImageDetailFragment();
+
+		final Bundle args = new Bundle();
+		args.putString("url", imageUrl);
+		f.setArguments(args);
+
+		return f;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mImageUrl = getArguments() != null ? getArguments().getString("url") : null;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final View v = inflater.inflate(R.layout.listview_image_detail_fragment, container, false);
+		mImageView = (ImageView) v.findViewById(R.id.image);
+
+		// 初始化背景图片
+		iv_image_bg = (ImageView) v.findViewById(R.id.iv_image_bg);
+
+		mAttacher = new PhotoViewAttacher(mImageView);
+
+		mAttacher.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+
+			@Override
+			public void onPhotoTap(View arg0, float arg1, float arg2) {
+				try {
+					getActivity().finish();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		progressBar = (ProgressBar) v.findViewById(R.id.loading);
+		return v;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		releaseBitmapLoadingListener = new ReleaseBitmapLoadingListener();
+		if (imageLoader == null)
+			imageLoader = ImageLoader.getInstance();
+		String path = mImageUrl;
+		File file = new File(mImageUrl);
+		if (file.isFile() && file.exists())
+			path = "file://" + mImageUrl;
+		imageLoader.displayImage(path, mImageView, releaseBitmapLoadingListener);
+	}
+
+	public class ReleaseBitmapLoadingListener extends SimpleImageLoadingListener {
+
+		private ArrayList<Bitmap> bitmapList = new ArrayList<Bitmap>();
+
+		public void cleanBitmapList() {
+			if (bitmapList.size() > 0) {
+				for (Bitmap bitmap : bitmapList) {
+					if (bitmap != null && !bitmap.isRecycled()) {
+						bitmap.recycle();
+					}
+				}
+			}
+			if (imageLoader != null) {
+				imageLoader.clearDiscCache();
+				imageLoader.clearMemoryCache();
+			}
+		}
+
+		@Override
+		public void onLoadingStarted(String imageUri, View view) {
+			progressBar.setVisibility(View.VISIBLE);
+			// wei
+			// 我自己写的
+			// 显示默认九宫格中的小图片
+//			Drawable bitmap = NoScrollGridAdapter.getBitmap();
+//			iv_image_bg.setBackgroundDrawable(bitmap);
+//			iv_image_bg.setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+			String message = null;
+			switch (failReason.getType()) {
+			case IO_ERROR:
+				message = "下载错误";
+				break;
+			case DECODING_ERROR:
+				message = "图片无法显示";
+				break;
+			case NETWORK_DENIED:
+				message = "网络有问题，无法下载";
+				break;
+			case OUT_OF_MEMORY:
+				message = "图片太大无法显示";
+				break;
+			case UNKNOWN:
+				message = "未知的错误";
+				break;
+			}
+			Toast.makeText(getContext(),message,Toast.LENGTH_SHORT).show();
+			progressBar.setVisibility(View.GONE);
+			iv_image_bg.setVisibility(View.GONE);
+		}
+
+		@Override
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+
+			progressBar.setVisibility(View.GONE);
+			iv_image_bg.setVisibility(View.GONE);
+			mAttacher.update();
+			bitmapList.add(loadedImage);
+		}
+	};
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (releaseBitmapLoadingListener != null) {
+			releaseBitmapLoadingListener.cleanBitmapList();
+		}
+	}
+}
