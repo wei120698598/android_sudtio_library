646127d05446d7243f278c442ad618bca3260e15
niwoshijie <me591091298@126.com>
2016-07-22 18:14:35 +0800

    测试提交

--- as_app_demo/src/main/java/com/test/lib/MainActivity.java
+++ as_app_demo/src/main/java/com/test/lib/MainActivity.java
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textList.add("图片选择呵呵呵呵呵呵呵呵呵哈哈哈哈呵呵");
+        textList.add("图片选择呵呵呵呵呵呵呵呵呵哈哈哈哈呵呵");
+
 
         listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, textList));
