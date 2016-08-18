878c5c45a38fc0171ef3738f0780b040d8c8dd68
wei120698598 <120698598@qq.com>
2016-08-18 18:30:21 +0800

    添加图片与base64互转工具类

---
+++ as_lib_ImageUtils/src/main/java/com/wei/image/imageUtils/ReadImgToBinary2.java
@@ -0,0 +1,103 @@
+package com.wei.image.imageUtils;
+
+/**
+ * Created by Wei on 2016/8/18.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 图片和base64互转
+ *
+ */
+public class ReadImgToBinary2 {
+
+    /**
+     * @param imgPath
+     * @param bitmap
+     * @param imgFormat 图片格式
+     * @return
+     */
+    public static String imgToBase64(String imgPath, Bitmap bitmap, String imgFormat) {
+        if (imgPath != null && imgPath.length() > 0) {
+            bitmap = readBitmap(imgPath);
+        }
+        if (bitmap == null) {
+            //bitmap not found!!
+        }
+        ByteArrayOutputStream out = null;
+        try {
+            out = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            out.flush();
+            out.close();
+
+            byte[] imgBytes = out.toByteArray();
+            return Base64.encodeToString(imgBytes, Base64.DEFAULT);
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+    private static Bitmap readBitmap(String imgPath) {
+        try {
+            return BitmapFactory.decodeFile(imgPath);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    /**
+     * @param base64Data
+     * @param imgName
+     * @param imgFormat  图片格式
+     */
+    public static void base64ToBitmap(String base64Data, String imgName, String imgFormat) {
+        byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+        File myCaptureFile = new File("/sdcard/", imgName);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myCaptureFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        boolean isTu = bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        if (isTu) {
+            // fos.notifyAll();
+            try {
+                fos.flush();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
